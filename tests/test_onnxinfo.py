08e19ec554944d29080ff07f30ac8a06a4b7b8d6
Ray Huang <bjhuang@cs.nycu.edu.tw>
2024-11-25 11:14:01 +0800

    feat: wget model & run tests on it

--- tests/test_onnxinfo.py
+++ tests/test_onnxinfo.py
@@ -6,12 +6,12 @@ def test_read_onnx_nofile():
         _onnxinfo.read_onnx('non-existing.onnx')
 
 def test_read_onnx():
-    info = _onnxinfo.read_onnx('models/resnet50-new.onnx')
+    info = _onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
     assert info is not None
 
 def test_iterate_onnx():
     try:
-        graph = _onnxinfo.read_onnx('models/resnet50-new.onnx')
+        graph = _onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
         _onnxinfo.iterate_graph(graph)
     except:
         pytest.fail("iterate_graph failed")
