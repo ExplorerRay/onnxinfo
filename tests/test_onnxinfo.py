b5af2bfbc533a01e40f795b263134dc4efe04efc
Ray Huang <bjhuang@cs.nycu.edu.tw>
2024-12-11 09:05:05 +0800

    fix: revise Python module name

--- tests/test_onnxinfo.py
+++ tests/test_onnxinfo.py
@@ -1,17 +1,17 @@
-import _onnxinfo
+import onnxinfo
 import pytest
 
 def test_read_onnx_nofile():
     with pytest.raises(ValueError):
-        _onnxinfo.read_onnx('non-existing.onnx')
+        onnxinfo.read_onnx('non-existing.onnx')
 
 def test_read_onnx():
-    info = _onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
+    info = onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
     assert info is not None
 
 def test_infer_shape():
-    model = _onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
-    target = _onnxinfo.InferShapeImpl(model.graph())
+    model = onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
+    target = onnxinfo.InferShapeImpl(model.graph())
     old_size = len(target.get_ndname_to_shape())
     target.infer_shapes()
     new_size = len(target.get_ndname_to_shape())
@@ -19,8 +19,8 @@ def test_infer_shape():
 
 def test_print_summary():
     try:
-        model = _onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
-        target = _onnxinfo.InferShapeImpl(model.graph())
+        model = onnxinfo.read_onnx('models/resnet18_Opset16.onnx')
+        target = onnxinfo.InferShapeImpl(model.graph())
         target.infer_shapes()
         target.print_summary()
     except:
