3d133e5fa5abcf59db9fdbdb7da3c66dcf63a5e8
Ray Huang <bjhuang@cs.nycu.edu.tw>
2024-12-13 10:25:10 +0800

    refactor: combine anal args into struct for functions

--- include/ModelAnalysis.hpp
+++ include/ModelAnalysis.hpp
@@ -23,14 +23,18 @@ struct NodeAnalArgs {
 };
 
 int64_t get_prod(std::vector<int64_t> &vec);
-NodeAnalArgs get_anal_args(onnx::NodeProto &node, const str_shape_map_t &ndname_to_shape, const str_sz_map_t &ndname_to_dtype_size);
+NodeAnalArgs get_anal_args(onnx::NodeProto &node,
+  const str_shape_map_t &shape_map,
+  const str_sz_map_t &sz_map);
 
-AnalyzeData analyze_node_Conv(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size);
-AnalyzeData analyze_node_Relu(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size);
-AnalyzeData analyze_node_MaxPool(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size);
-AnalyzeData analyze_node_Add(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size);
-AnalyzeData analyze_node_GlobalAveragePool(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size);
-AnalyzeData analyze_node_Flatten(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size);
-AnalyzeData analyze_node_Gemm(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size);
+AnalyzeData analyze_node_Conv(onnx::NodeProto &node, NodeAnalArgs &anal_args);
+AnalyzeData analyze_node_Relu(onnx::NodeProto &node, NodeAnalArgs &anal_args);
+AnalyzeData analyze_node_MaxPool(onnx::NodeProto &node, NodeAnalArgs &anal_args);
+AnalyzeData analyze_node_Add(onnx::NodeProto &node, NodeAnalArgs &anal_args);
+AnalyzeData analyze_node_GlobalAveragePool(onnx::NodeProto &node, NodeAnalArgs &anal_args);
+AnalyzeData analyze_node_Flatten(onnx::NodeProto &node, NodeAnalArgs &anal_args);
+AnalyzeData analyze_node_Gemm(onnx::NodeProto &node, NodeAnalArgs &anal_args);
 
-AnalyzeData analyze_node(onnx::NodeProto &node, const str_shape_map_t &ndname_to_shape, const str_sz_map_t &ndname_to_dtype_size);
+AnalyzeData analyze_node(onnx::NodeProto &node,
+  const str_shape_map_t &ndname_to_shape,
+  const str_sz_map_t &ndname_to_dtype_size);
--- src/ModelAnalysis.cpp
+++ src/ModelAnalysis.cpp
@@ -9,16 +9,19 @@ int64_t get_prod(std::vector<int64_t> &vec) {
   return prod;
 }
 
-NodeAnalArgs get_anal_args(onnx::NodeProto &node, const str_shape_map_t &ndname_to_shape, const str_sz_map_t &ndname_to_dtype_size) {
+NodeAnalArgs get_anal_args(onnx::NodeProto &node,
+  const str_shape_map_t &shape_map,
+  const str_sz_map_t &sz_map)
+{
   NodeAnalArgs anal_args;
   std::vector<std::vector<int64_t>> input_shapes;
   str_sz_map_t ndname_to_size;
   for (auto input : node.input()) {
-    input_shapes.emplace_back(ndname_to_shape.at(input));
-    ndname_to_size[input] = ndname_to_dtype_size.at(input);
+    input_shapes.emplace_back(shape_map.at(input));
+    ndname_to_size[input] = sz_map.at(input);
   }
-  std::vector<int64_t> output_shape = ndname_to_shape.at(node.output(0));
-  ndname_to_size[node.output(0)] = ndname_to_dtype_size.at(node.output(0));
+  std::vector<int64_t> output_shape = shape_map.at(node.output(0));
+  ndname_to_size[node.output(0)] = sz_map.at(node.output(0));
 
   anal_args.input_shapes = input_shapes;
   anal_args.output_shape = output_shape;
@@ -27,8 +30,11 @@ NodeAnalArgs get_anal_args(onnx::NodeProto &node, const str_shape_map_t &ndname_
   return anal_args;
 }
 
-AnalyzeData analyze_node_Conv(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size) {
+AnalyzeData analyze_node_Conv(onnx::NodeProto &node, NodeAnalArgs &anal_args) {
   AnalyzeData data;
+  std::vector<std::vector<int64_t>> input_shapes = anal_args.input_shapes;
+  std::vector<int64_t> output_shape = anal_args.output_shape;
+  str_sz_map_t ndname_to_size = anal_args.ndname_to_size;
 
   // MACs
   std::vector<int64_t> kernel_shape = input_shapes[1];
@@ -51,8 +57,11 @@ AnalyzeData analyze_node_Conv(onnx::NodeProto &node, std::vector<std::vector<int
   return data;
 }
 
-AnalyzeData analyze_node_Relu(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size) {
+AnalyzeData analyze_node_Relu(onnx::NodeProto &node, NodeAnalArgs &anal_args) {
   AnalyzeData data;
+  std::vector<std::vector<int64_t>> input_shapes = anal_args.input_shapes;
+  std::vector<int64_t> output_shape = anal_args.output_shape;
+  str_sz_map_t ndname_to_size = anal_args.ndname_to_size;
 
   data.param = 0; // no trainable parameters
   data.mac = 0; // non MACs operation
@@ -66,8 +75,11 @@ AnalyzeData analyze_node_Relu(onnx::NodeProto &node, std::vector<std::vector<int
   return data;
 }
 
-AnalyzeData analyze_node_MaxPool(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size) {
+AnalyzeData analyze_node_MaxPool(onnx::NodeProto &node, NodeAnalArgs &anal_args) {
   AnalyzeData data;
+  std::vector<std::vector<int64_t>> input_shapes = anal_args.input_shapes;
+  std::vector<int64_t> output_shape = anal_args.output_shape;
+  str_sz_map_t ndname_to_size = anal_args.ndname_to_size;
 
   // MACs
   data.mac = 0; // non MACs operation
@@ -82,8 +94,11 @@ AnalyzeData analyze_node_MaxPool(onnx::NodeProto &node, std::vector<std::vector<
   return data;
 }
 
-AnalyzeData analyze_node_Add(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size) {
+AnalyzeData analyze_node_Add(onnx::NodeProto &node, NodeAnalArgs &anal_args) {
   AnalyzeData data;
+  std::vector<std::vector<int64_t>> input_shapes = anal_args.input_shapes;
+  std::vector<int64_t> output_shape = anal_args.output_shape;
+  str_sz_map_t ndname_to_size = anal_args.ndname_to_size;
 
   // MACs
   for (size_t i = 0; i < input_shapes.size(); ++i) {
@@ -100,8 +115,11 @@ AnalyzeData analyze_node_Add(onnx::NodeProto &node, std::vector<std::vector<int6
   return data;
 }
 
-AnalyzeData analyze_node_GlobalAveragePool(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size) {
+AnalyzeData analyze_node_GlobalAveragePool(onnx::NodeProto &node, NodeAnalArgs &anal_args) {
   AnalyzeData data;
+  std::vector<std::vector<int64_t>> input_shapes = anal_args.input_shapes;
+  std::vector<int64_t> output_shape = anal_args.output_shape;
+  str_sz_map_t ndname_to_size = anal_args.ndname_to_size;
 
   // MACs
   for (size_t i = 0; i < input_shapes.size(); ++i) {
@@ -119,8 +137,11 @@ AnalyzeData analyze_node_GlobalAveragePool(onnx::NodeProto &node, std::vector<st
   return data;
 }
 
-AnalyzeData analyze_node_Flatten(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size) {
+AnalyzeData analyze_node_Flatten(onnx::NodeProto &node, NodeAnalArgs &anal_args) {
   AnalyzeData data;
+  std::vector<std::vector<int64_t>> input_shapes = anal_args.input_shapes;
+  std::vector<int64_t> output_shape = anal_args.output_shape;
+  str_sz_map_t ndname_to_size = anal_args.ndname_to_size;
 
   // MACs
   data.mac = 0; // non MACs operation
@@ -135,8 +156,11 @@ AnalyzeData analyze_node_Flatten(onnx::NodeProto &node, std::vector<std::vector<
   return data;
 }
 
-AnalyzeData analyze_node_Gemm(onnx::NodeProto &node, std::vector<std::vector<int64_t>> &input_shapes, std::vector<int64_t> &output_shape, str_sz_map_t &ndname_to_size) {
+AnalyzeData analyze_node_Gemm(onnx::NodeProto &node, NodeAnalArgs &anal_args) {
   AnalyzeData data;
+  std::vector<std::vector<int64_t>> input_shapes = anal_args.input_shapes;
+  std::vector<int64_t> output_shape = anal_args.output_shape;
+  str_sz_map_t ndname_to_size = anal_args.ndname_to_size;
 
   // MACs
   data.mac = get_prod(input_shapes[0]) * get_prod(output_shape) * MUL_MACS;
@@ -151,29 +175,32 @@ AnalyzeData analyze_node_Gemm(onnx::NodeProto &node, std::vector<std::vector<int
   return data;
 }
 
-AnalyzeData analyze_node(onnx::NodeProto &node, const str_shape_map_t &ndname_to_shape, const str_sz_map_t &ndname_to_dtype_size) {
+AnalyzeData analyze_node(onnx::NodeProto &node,
+  const str_shape_map_t &ndname_to_shape,
+  const str_sz_map_t &ndname_to_dtype_size)
+{
   AnalyzeData data;
   NodeAnalArgs anal_args = get_anal_args(node, ndname_to_shape, ndname_to_dtype_size);
   if (node.op_type() == "Conv") {
-    data = analyze_node_Conv(node, anal_args.input_shapes, anal_args.output_shape, anal_args.ndname_to_size);
+    data = analyze_node_Conv(node, anal_args);
   }
   else if (node.op_type() == "Relu") {
-    data = analyze_node_Relu(node, anal_args.input_shapes, anal_args.output_shape, anal_args.ndname_to_size);
+    data = analyze_node_Relu(node, anal_args);
   }
   else if (node.op_type() == "MaxPool") {
-    data = analyze_node_MaxPool(node, anal_args.input_shapes, anal_args.output_shape, anal_args.ndname_to_size);
+    data = analyze_node_MaxPool(node, anal_args);
   }
   else if (node.op_type() == "Add") {
-    data = analyze_node_Add(node, anal_args.input_shapes, anal_args.output_shape, anal_args.ndname_to_size);
+    data = analyze_node_Add(node, anal_args);
   }
   else if (node.op_type() == "GlobalAveragePool") {
-    data = analyze_node_GlobalAveragePool(node, anal_args.input_shapes, anal_args.output_shape, anal_args.ndname_to_size);
+    data = analyze_node_GlobalAveragePool(node, anal_args);
   }
   else if (node.op_type() == "Flatten") {
-    data = analyze_node_Flatten(node, anal_args.input_shapes, anal_args.output_shape, anal_args.ndname_to_size);
+    data = analyze_node_Flatten(node, anal_args);
   }
   else if (node.op_type() == "Gemm") {
-    data = analyze_node_Gemm(node, anal_args.input_shapes, anal_args.output_shape, anal_args.ndname_to_size);
+    data = analyze_node_Gemm(node, anal_args);
   }
   else {
     std::cerr << "Error: " << node.op_type() << " not supported now\n";
